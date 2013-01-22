ac45e8a40077116d650dadf5e5a652ce3dfb9d16
Silas Villon <silas.villon@corp.globo.com>
2013-01-22 17:16:03 -0200

    banco assincrono

--- blog.py
+++ blog.py
@@ -3,32 +3,70 @@
 import tornado.database
 import tornado.ioloop
 import tornado.web
+import tornado.httpclient
+
+# from MySQLdb.connections import AsyncConnection
+
+from adisp import process
+from adb import Database
+
+class BaseHandler(tornado.web.RequestHandler):
+
+    adb = Database(
+                   driver="MySQLdb",
+                   host='localhost',
+                   database='blog',
+                   user='root',
+                   password='',
+                   num_threads=3,
+                   tx_connection_pool_size=2,
+                   queue_timeout=0.001)
+
+    db = tornado.database.Connection(
+                   host='localhost',
+                   database='blog',
+                   user='root')
 
 
-class MainHandler(tornado.web.RequestHandler):
+class MainHandler(BaseHandler):
 
+    # @tornado.web.asynchronous
+    # def get(self):
+    #     self.entries = self.adb.async_query("select * from articles order by id desc;",
+    #                                    self.query_done)
+    #     # client = tornado.httpclient.AsyncHTTPClient()
+    #     # client.fetch("http://localhost:8888/story/1", self.query_done)
+
+    # def query_done(self, *stuff):
+    #     # self.finish(self.render("index.html", entries = stuff[0]))
+    #     self.write('success!!')
+    #     self.finish()
+
+
+    @process
+    @tornado.web.asynchronous
     def get(self):
-        db = tornado.database.Connection(host='localhost', database='blog', user='root')
-        entries = db.query("select * from articles order by id desc;")
+        entries = yield self.adb.runQuery("select * from articles order by id desc;")
         self.render("index.html", entries = entries)
 
 
-class EntryHandler(tornado.web.RequestHandler):
+class EntryHandler(BaseHandler):
 
+    @process
+    @tornado.web.asynchronous
     def get(self, story_id):
-        db = tornado.database.Connection(host='localhost', database='blog', user='root')
-        entry = db.get("select * from articles where id=" + story_id)
-
-        self.render("entry.html", entry=entry)
+        entry = yield self.adb.runQuery("select * from articles where id=" + story_id)
+        self.render("entry.html", entry=entry[0])
 
 
-class NewPostHandler(tornado.web.RequestHandler):
+class NewPostHandler(BaseHandler):
 
     def get(self):
         self.render("new.html")
 
+    @process
+    @tornado.web.asynchronous
     def post(self):
-        db = tornado.database.Connection(host='localhost', database='blog', user='root')
         query = u"""
                 insert into articles (title, subtitle, body, creation_date)
                 values('%(TITLE)s', '%(SUBTITLE)s', '%(BODY)s', '%(CREATION_DATE)s');
@@ -38,7 +76,8 @@ class NewPostHandler(tornado.web.RequestHandler):
                     'BODY': self.get_argument('body'),
                     'CREATION_DATE': self.get_argument('creation-date')
                 }
-        executed = db.execute(query)
+        executed = yield self.adb.runOperation(query)
+        import pdb; pdb.set_trace()
         self.redirect('/story/%s' % executed)
 
 
